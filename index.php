30bda229270838c506fd514f722a28293d524edc
reyna lidia chambilla chura <reynachambilla@gmail.com>
2016-03-06 19:10:17 -0500

    pagina

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div>
+		<nav>
+			<ul class="menu-list">
+				<li class="menu-item"><a href="" class="menu-link">EMPRESA DE PANTALONES PARA DAMAS</a></li>
+			</ul>
+		</nav>
+		<div>
+	<img class="img" src="lidia.jpg" alt="mi imagen" height="500" width="1330">
+	    <article>
+   	
+          <p>es una empresa que brinda fiversos modelos de ropas especialmente en jeans para damas de buen calidad que ofrece de diferentes telas como indigo, fraisterry, dril, etc. brida servicios para diferentes empresas de peru.
+          </p>
+   <div class="flexslider">
+       <ul class="slides">
+   	       <img src="chura.jpg" alt="" height="300" width="400">
+           <img src="nery.jpg" alt="" height="300" width="400">
+           <img src="reyna.jpg" alt="" height="300" width="400">
+       </div>	
+
+
+<video controls width="420" height="240">
+        <source src="mi video.mp4" type="video/mp4" media="">  
+        <source src="Cómo Vender Un Producto A Un Cliente-  3 tácticas importantísimas para vender un producto.mp4" type="video/.ogg" media="">  
+        <source src="BODEGAS PANTALON LEVIS LOS ANGELES CA.mp4" type="video/webm" media="">  
+</video>
+  <iframe width="320" height="240" src="https://www.youtube.com/watch?v=7YamLYhNxRk" frameborder="0" allowfullscreen></iframe>
+
+  <iframe width="320" height="240" src="https://www.youtube.com/watch?v=byPmnDpWxoI" frameborder="0" allowfullscreen></iframe>
+
+</div>
+
+</body>
+</html>
